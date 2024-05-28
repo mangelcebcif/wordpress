30f05b5980b7a2c623175c24b300f78b137563b5
mangelcebcif <migcebcif@alu.edu.gva.es>
2024-05-28 13:43:27 +0200

    gdffdhfd

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "prueba de visualizacion";
+echo "prueba de visualizacion 2222222232";
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
