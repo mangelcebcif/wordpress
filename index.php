23f09f843085422eb3c82189224f5828f9cef861
mangelcebcif <migcebcif@alu.edu.gva.es>
2024-06-09 12:05:29 +0200

    PRE-PRESENTACIÓN

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "Esta parte se debe cambiar en la presentación del proyecto. esto es una prueba";
+echo "Esta parte se debe cambiar en la presentación del proyecto.";
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
